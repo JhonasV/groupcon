7ec9003dd2426ed89489bd695e6b9173d28e292d
JhonasV <jhonas@gmail.com>
2019-09-24 23:10:34 -0400

    prod.js credentials removed

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,14 +1,3 @@
-// const mongo_credentiasl = {
-//   user: "jhonasv",
-//   password: "hola1234",
-//   db: "groupcon"
-// };
-// module.exports = {
-//   // DATABASE_URI: "mongodb://127.0.0.1:27017/groupcon",
-//   DATABASE_URI: `mongodb://${mongo_credentiasl.user}:${mongo_credentiasl.password}@ds145146.mlab.com:45146/${mongo_credentiasl.db}`,
-//   JWT_KEY: "dsadasd4sa65d4as56d46as5d46as5d46a5s"
-// };
-
 module.exports = {
   DATABASE_URI: process.env.DATABSE_URI,
   JWT_KEY: process.env.JWT_KEY,
