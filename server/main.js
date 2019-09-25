94e9fbc0c24b2f2c0c48128ec986dffb512cd791
JhonasV <jhonas@gmail.com>
2019-09-24 22:15:34 -0400

    client configuration

--- server/main.js
+++ server/main.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
-const morgan = require("morgan");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
+
 require("./data/database");
 require("./models");
 require("./routes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+} else {
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
