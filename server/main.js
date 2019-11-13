5a3ad5dfb9d2611d47a26ca2c8094cb9cd1a54b1
JhonasV <jhonas@gmail.com>
2019-11-13 07:22:39 -0400

    cors enable for dev

--- server/main.js
+++ server/main.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 require("./data/database");
 require("./models");
